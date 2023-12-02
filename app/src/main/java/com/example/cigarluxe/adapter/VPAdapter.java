f9ed1ed95a531dbe665165b8f3e9538703813c1d
Brendon Mehdi <brendonmehdi@gmail.com>
2023-12-02 14:32:24 -0500

    #7 ADDED CONTENT TO VIEWPAGER

--- app/src/main/java/com/example/cigarluxe/adapter/VPAdapter.java
+++ app/src/main/java/com/example/cigarluxe/adapter/VPAdapter.java
@@ -18,10 +18,10 @@ public class VPAdapter extends FragmentStateAdapter {
     @Override
     public Fragment createFragment(int position) {
         switch (position){
-            case 0: return VpLayout.newInstance(R.drawable.money1,  "1. Cut the Cigar", "Shoot for 1/8th of an inch you just want to cut the cap");
-            case 1: return VpLayout.newInstance(R.drawable.strength1, "2. Toast the Foot", "holding the cigar at a 46deg angle apply heat to the foot");
-            case 2: return VpLayout.newInstance(R.drawable.aboutashton, "3. Light the Cigar", "without letting the flame touch the cigar puff a few times");
-            case 3: return VpLayout.newInstance(R.drawable.cigar, "4. Make sure its evenly lit", "look at the foot of the cigar and lightly blow on it to ensure its evenly lit");
+            case 0: return VpLayout.newInstance(R.drawable.cutcigar,  "1. Cut the Cigar", "Shoot for 1/8th of an inch you just want to cut the cap");
+            case 1: return VpLayout.newInstance(R.drawable.toastcigar, "2. Toast the Foot", "holding the cigar at a 46deg angle apply heat to the foot");
+            case 2: return VpLayout.newInstance(R.drawable.lightcigar, "3. Light the Cigar", "without letting the flame touch the cigar puff a few times");
+            case 3: return VpLayout.newInstance(R.drawable.evenlylit, "4. Make sure its evenly lit", "look at the foot of the cigar and lightly blow on it to ensure its evenly lit");
             case 4: return VpLayout.newInstance(R.drawable.cigarpagerenjoy, "5. Smoke and Enjoy", "Smoke without inhaling and puff every 30 - 60 sec");
             default: return VpLayout.newInstance(R.drawable.aboutcohiba, "404: Fragment not found", "hi");
         }
