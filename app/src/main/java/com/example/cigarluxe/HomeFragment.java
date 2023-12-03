a9ea3e9d516893093410a303c0e08ec4a0cf9f9e
Brendon Mehdi <brendonmehdi@gmail.com>
2023-12-03 15:18:36 -0500

    MINOR CHANGES

--- app/src/main/java/com/example/cigarluxe/HomeFragment.java
+++ app/src/main/java/com/example/cigarluxe/HomeFragment.java
@@ -3,9 +3,11 @@ package com.example.cigarluxe;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 
